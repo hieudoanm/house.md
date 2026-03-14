ca16e341259c47ddabcb353e11c5bb033b0de8e4
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-14 20:18:51 +0700

    update prettier

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -5,24 +5,24 @@ import Head from 'next/head';
 import { FC } from 'react';
 
 const robotoCondensed = Roboto_Condensed({
-  subsets: ['latin'],
-  weight: ['400', '700'],
+	subsets: ['latin'],
+	weight: ['400', '700'],
 });
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <title>House, M.D.</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <div className={robotoCondensed.className}>
-        <Component {...pageProps} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>House, M.D.</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<div className={robotoCondensed.className}>
+				<Component {...pageProps} />
+			</div>
+		</>
+	);
 };
 
 export default App;
